3e70534fe82089822f54b65d03d063d82cf10840
Juan Tito <juangtito.hck@gmail.com>
2025-10-03 11:36:44 -0500

    update png icon

--- app/page.js
+++ app/page.js
@@ -28,9 +28,14 @@ export const metadata = {
     type: "website",
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-32x32.png",
-    apple: "/favicon-180x180.png", // para iOS
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
   },
 };
 
